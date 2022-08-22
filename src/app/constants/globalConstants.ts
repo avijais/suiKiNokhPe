8d7ab241ab20db65fa2957f6a28a1e72b8c19ed7
cakeforu <cakeforu@outlook.com>
2022-08-22 20:12:41 +0530

    outside globalcons file deleted and inside app globalConst file updated

--- src/app/constants/globalConstants.ts
+++ src/app/constants/globalConstants.ts
@@ -1,16 +1,18 @@
 'use strict';
 
 export class GlobalConstants {
-    public static API_URL = 'https://api.downloadfood.cladev.com/api/v1.0/'
+    public static API_URL = 'https://api.test.com/api/v1.0/';
     public static INFO_MSG =  {
         PASWRD_FORMT_ERR: 'Password must contain at least (6) characters, including (1) Number, (1) UPPERCASE, and (1) lowercase letter',
-        VLID_USRNEM: 'Please enter a valid email address.',
+        VLID_USRNEM: 'Please enter a valid email address.'
     };
     public static REDIRECT_URLS = {
         DASHBOARD: '/dashboard',
         LOGIN: '/'
     };
     public static API = {
-        LOGIN: "useractivity/"
-    }
+        LOGIN: "useractivity/",
+        REST_DROP_DOWN: "/",
+        USER_TYPE: "usertype/"
+    };
 }
